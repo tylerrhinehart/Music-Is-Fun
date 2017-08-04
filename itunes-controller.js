6d8bfb66c3c1784c6e3a5e3ffb2b8eccbd522206
Tyler Rhinehart <rhinehart22@gmail.com>
2017-08-03 19:10:23 -0600

    added listener to stop multiple songs from playing at once

--- itunes-controller.js
+++ itunes-controller.js
@@ -1,20 +1,21 @@
-function ItunesController(){
+function ItunesController() {
   var itunesService = new ItunesService()
   //Do Not Modify the getMusic function
-  this.getMusic = function getMusic(e){
+  this.getMusic = function getMusic(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
     itunesService.getMusicByArtist(artist).then(draw); //after get music by artist returns what are you doing with the objects?
   }
 
   //Start coding here
-  //Make it not skip songs
   function draw(artist) {
     var template = ''
     var rowTemplate = '<div class="row">'
-    for(var i = 1; i < artist.length+1; i++) {
-      var item = artist[i-1]
-      if(i % 3 != 0) {
+    var count = 1
+    var id = 1
+    for (var i = 0; i < artist.length; i++) {
+      var item = artist[i]
+      if (count <= 3) {
         rowTemplate += `
         <div class="col-xs-12 col-md-4">
               <div class="card thumbnail">
@@ -24,37 +25,33 @@ function ItunesController(){
                   <h4>${item.artist}</h4>
                   <h5>${item.collection}</h5>
                   <p>$${item.price}</p>
-                  <audio controls class="audio-controls">
+                  <audio controls class="audio-controls" id="${id}">
                     <source src="${item.preview}" type="audio/mp4" />
                 </div>
               </div>
           </div>
           `
+        count++
+        id++
       }
       else {
-        rowTemplate += `
-        <div class="col-xs-12 col-md-4">
-              <div class="card thumbnail">
-                <img src="${item.albumArt}" alt="albumArt">
-                <div class="card-body">
-                  <h4>${item.title}</h4>
-                  <h4>${item.artist}</h4>
-                  <h5>${item.collection}</h5>
-                  <p>$${item.price}</p>
-                  <audio controls class="audio-controls">
-                    <source src="${item.preview}" type="audio/mp4" />
-                </div>
-              </div>
-          </div>
-          `
+        count = 1
         template += rowTemplate + '</div>'
         rowTemplate = '<div class="row">'
       }
     }
     document.getElementById('songs').innerHTML = template
+
   }
 
+  document.addEventListener('play', function (e) {
+    console.log(e)
+    var players = document.getElementsByClassName('audio-controls')
+    for (var i = 0; i < players.length; i++) {
+      if (players[i] != e.target) {
+        players[i].pause()
+      }
+    }
+  }, true);
 
-
-  
 }
